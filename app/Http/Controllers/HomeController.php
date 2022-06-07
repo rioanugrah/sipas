f5bec9f49e18480f8baec9e780353adf23f81b41
Rio Anugrah Adam Saputra <rioanugrah999@gmail.com>
2022-06-07 21:50:53 +0700

    Sipas Update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('backend.home');
+        // return view('home');
     }
 }
---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Roles;
+use \Carbon\Carbon;
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Roles::truncate();
+
+        $roles = [
+            [
+              'id' => 1,
+              'nama_role' => 'Administrator',
+              'created_at' => Carbon::now(),
+              'updated_at' => Carbon::now(),
+            ],
+            [
+              'id' => 2,
+              'nama_role' => 'Admin',
+              'created_at' => Carbon::now(),
+              'updated_at' => Carbon::now(),
+            ],
+            [
+              'id' => 3,
+              'nama_role' => 'User',
+              'created_at' => Carbon::now(),
+              'updated_at' => Carbon::now(),
+            ],
+            [
+              'id' => 4,
+              'nama_role' => 'Komisaris Utama',
+              'created_at' => Carbon::now(),
+              'updated_at' => Carbon::now(),
+            ],
+            [
+              'id' => 5,
+              'nama_role' => 'Direktur',
+              'created_at' => Carbon::now(),
+              'updated_at' => Carbon::now(),
+            ],
+            [
+              'id' => 6,
+              'nama_role' => 'Sekretaris',
+              'created_at' => Carbon::now(),
+              'updated_at' => Carbon::now(),
+            ],
+        ];
+
+        Roles::insert($roles);
+    }
+}
